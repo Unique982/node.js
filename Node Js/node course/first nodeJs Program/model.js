433fe6faf18eac022f5b34f546ee4dd8382ccc44
Unique Neupane <khemrajneupane111@gmail.com>
2025-03-14 21:18:06 +0545

    Delete Node Js directory

--- Node Js/node course/first nodeJs Program/model.js
+++
@@ -1,6 +0,0 @@
-//Modules
-// common js (module.exports,require);
-// EcmaScript module (import, export default, export)
-const {add,sub} = require('./app');
-console.log(1,99);
-console.log(1,99);
--- Node Js/node course/first.js
+++
@@ -1,7 +0,0 @@
-console.log("hello");
-const fs = require('fs');
-fs.writeFile('Output.txt',"Write the output testing", (err)=>{
-  if(err) throw err;
-  else console.log("Successfully");
-  console.log(__filename);
-})
