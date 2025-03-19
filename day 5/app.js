1f260720b937ea731a0109e7e0ab215d418c031e
Unique Neupane <khemrajneupane111@gmail.com>
2025-03-19 20:36:28 +0545

    Add files via upload
    Upload code day 5

---
+++ day 5/app.js
@@ -0,0 +1,16 @@
+// syntax error
+console.log("heeloo");
+// run time error
+// console.log(x);
+// const num =100;
+// num();
+
+// logical error
+
+let num = 100;
+if (num = 1000) {
+  console.log(num);
+}
+else{
+  console.log("num is no 1000");
+}
